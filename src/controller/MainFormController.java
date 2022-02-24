e9850d07fcf9dcf03ab9cc34e19ea17d14bf24ec
LahiruRJ <sanlahiru720@gmail.com>
2022-02-25 01:18:10 +0530

    Add forms

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class MainFormController {
+    public TextArea txtEditor;
+    public Button btnRun;
+    public TextArea txtOutput;
+
+    public void btnRun_OnAction(ActionEvent event) {
+
+    }
 }

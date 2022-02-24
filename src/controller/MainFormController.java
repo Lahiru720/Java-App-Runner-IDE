26c95d365c92b2611ebc668c83240a2bf4777682
LahiruRJ <sanlahiru720@gmail.com>
2022-02-25 03:09:16 +0530

    Path assign

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -4,13 +4,26 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MainFormController {
     public TextArea txtEditor;
     public Button btnRun;
     public TextArea txtOutput;
 
     public void btnRun_OnAction(ActionEvent event) {
+    try{
         String data = "public class DEP8IDEDemo{\n" + "public static void main(String args[]){\n" + txtEditor.getText() + "\n}\n" + "}";
+
+        String tempDir = System.getProperty("java.io.tmpdir");
+        Path tempFilePath = Paths.get(tempDir, "DEP8IDEDemo.java");
+        Files.write(tempFilePath, data.getBytes());
+    }catch(IOException e){
+        e.printStackTrace();
+    }
 
     }
 }

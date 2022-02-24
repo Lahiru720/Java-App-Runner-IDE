dc3a8219d06dfc06687f0f0e1176174a8d929ffc
LahiruRJ <sanlahiru720@gmail.com>
2022-02-25 03:23:32 +0530

    Add readStream method

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,18 +17,32 @@ public class MainFormController {
 
     public void btnRun_OnAction(ActionEvent event) {
     try{
-        String data = "public class DEP8IDEDemo{\n" + "public static void main(String args[]){\n" + txtEditor.getText() + "\n}\n" + "}";
+        String data = "public class JavaAppRunner{\n" + "public static void main(String args[]){\n" + txtEditor.getText() + "\n}\n" + "}";
 
         String tempDir = System.getProperty("java.io.tmpdir");
-        Path tempFilePath = Paths.get(tempDir, "DEP8IDEDemo.java");
+        Path tempFilePath = Paths.get(tempDir, "JavaAppRunner.java");
         Files.write(tempFilePath, data.getBytes());
 
         Process javac = Runtime.getRuntime().exec("javac " + tempFilePath);
         int exitCode = javac.waitFor();
 
+        if(exitCode==0){
+            Process java = Runtime.getRuntime().exec("java -cp " + tempDir + "JavaAppRunner");
+            exitCode=java.waitFor();
+        }else{
+
+        }
+
     }catch(IOException | InterruptedException e){
         e.printStackTrace();
     }
 
+    }
+
+    public void readStream(InputStream is) throws IOException {
+        byte[] buffer=new byte[is.available()];
+        is.read(buffer);
+        txtOutput.setText(new String(buffer));
+        is.close();
     }
 }

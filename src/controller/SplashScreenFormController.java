b48e892ec42f87092a1304f99ab23f7aa30bc117
Lahiru Rajapakshe <66423576+Lahiru720@users.noreply.github.com>
2022-02-27 22:41:55 +0530

    Remove thread

--- src/controller/SplashScreenFormController.java
+++ src/controller/SplashScreenFormController.java
@@ -27,45 +27,13 @@ public class SplashScreenFormController {
         lblStatus.textProperty().bind(statusText);
         pgb.progressProperty().bind(progress);
 
-        new Thread(() -> {
-            try {
-//                Connection connection = establishDBConnection();
-                updateProgress("Stating the app", 1.0);
-                Thread.sleep(100);
-//                DBConnection.getInstance().init(connection);connection
-
-                AnchorPane root = FXMLLoader.load(this.getClass().getResource("/view/LoginForm.fxml"));
-                Platform.runLater(() -> {
-                    ((Stage) (pgb.getScene().getWindow())).close();
-                    Stage stage = new Stage();
-                    Scene loginScene = new Scene(root);
-                    stage.setScene(loginScene);
-                    stage.setTitle("Hello JDBC: Login");
-                    stage.setResizable(false);
-                    stage.sizeToScene();
-                    stage.centerOnScreen();
-                    stage.show();
-                });
-            } catch (Throwable e) {
-                e.printStackTrace();
-                try {
-                    updateProgress("Failed to initialize", 0.8);
-                    Thread.sleep(100);
-
-                    updateProgress("Shutting down the app", 1.0);
-                    Thread.sleep(100);
-                    System.exit(0);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }).start();
+      
     }
 
 
     private void createDB() throws Throwable {
 
-       
+
     }
 
 

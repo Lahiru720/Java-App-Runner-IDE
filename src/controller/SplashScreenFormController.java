4da807aaa2de8d25e43279489db04be8499edf22
Lahiru Rajapakshe <66423576+Lahiru720@users.noreply.github.com>
2022-02-27 22:46:48 +0530

    Remove simple object property

--- src/controller/SplashScreenFormController.java
+++ src/controller/SplashScreenFormController.java
@@ -7,27 +7,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-//import util.DBConnection;DBConnection
-
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class SplashScreenFormController {
-    private final SimpleDoubleProperty progress = new SimpleDoubleProperty(0.0);
-    private final SimpleStringProperty statusText = new SimpleStringProperty("Initializing...");
-    public Label lblStatus;
-    public ProgressBar pgb;
+
 
     public void initialize() {
-        lblStatus.textProperty().bind(statusText);
-        pgb.progressProperty().bind(progress);
 
-      
+
     }
 
 
